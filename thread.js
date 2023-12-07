999b4227063a5b49527c3cbff7e5a24c8266dbc3
Jason <jasongtse@gmail.com>
2023-12-07 14:47:39 -0330

    Commit attempt 2: Added updated commeting code

--- thread.js
+++ thread.js
@@ -5,6 +5,8 @@ $(document).ready (() => {
     dbname = "ForumData";
     dbCollection = "ThreadData";
 
+    let commentSection = document.querySelector(".comments-list");
+
     
     connectToThreadsDB()
     .then(dataFromConnectAction => {
@@ -14,13 +16,74 @@ $(document).ready (() => {
         getCollectionData(dbCollection).then(data => {
             console.log("Data:", data);
             cardHeader = document.querySelector(".card-header");
-            cardHeader.innerText = data[1].title;
+            cardHeader.innerText = data[20].title;
             cardText = document.querySelector(".card-text");
-            cardText.innerText = data[1].content;
+            cardText.innerText = data[20].content;
             voteCount = document.querySelector(".vote-count");
-            voteCount.innerText = data[1].score;
-        });
+            voteCount.innerText = data[20].score;
+            const postButton = document.getElementById('postButton');
+            const commentInput = document.getElementById('commentInput');
+        
+            postButton.addEventListener('click', () => {
+                const newComment = commentInput.value;
+                if (newComment.trim() !== '') {
+                    data[20].comments.push(newComment);
+                    commentInput.value = '';
+                    updateCommentsList();
 
+                };
+                
+            });
+
+            function updateCommentsList() {
+                commentSection.innerHTML = '<h5>Comments:</h5><ul class="comments-list"></ul>';
+        
+                for (let i = 0; i < data[20].comments.length; i++) {
+                    let lineBreak = document.createElement("br");
+                    let commentElement = document.createElement("li");
+                    commentElement.innerText = data[20].comments[i];
+                    let thumbsUpButton = document.createElement("button");
+                    thumbsUpButton.className = 'btn btn-success up-vote';
+                    let thumbsUpButtonIcon = document.createElement("i");
+                    thumbsUpButtonIcon.className = 'fa-solid fa-thumbs-up';
+                    let thumbsDownButton = document.createElement("button");
+                    thumbsDownButton.className = 'btn btn-primary down-vote';
+                    let thumbsDownButtonIcon = document.createElement("i");
+                    thumbsDownButtonIcon.className = 'fa-solid fa-thumbs-down';
+                    thumbsUpButton.appendChild(thumbsUpButtonIcon);
+                    thumbsDownButton.appendChild(thumbsDownButtonIcon);
+        
+                    commentSection.appendChild(commentElement);
+                    commentSection.appendChild(lineBreak);
+                    commentSection.appendChild(thumbsUpButton);
+                    commentSection.appendChild(thumbsDownButton);
+                };
+            };
+        
+            
+
+            for (let i = 0; i < data[20].comments.size; i++){
+                let lineBreak = document.createElement("br");
+                let commentElement = document.createElement("li");
+                commentElement.innerText = data[20].comments[i];
+                let thumbsUpButton = document.createElement("button");
+                thumbsUpButton.className = 'btn btn-success up-vote';
+                let thumbsUpButtonIcon = document.createElement("i");
+                thumbsUpButtonIcon.className = 'fa-solid fa-thumbs-up';
+                let thumbsDownButton = document.createElement("button");
+                thumbsDownButton.className = 'btn btn-primary down-vote';
+                let thumbsDownButtonIcon = document.createElement("i");
+                thumbsDownButtonIcon.className = 'fa-solid fa-thumbs-down';
+                thumbsUpButton.appendChild(thumbsUpButtonIcon);
+                thumbsDownButton.appendChild(thumbsDownButtonIcon);
+
+
+                commentSection.appendChild(commentElement);
+                commentSection.appendChild(lineBreak);
+                commentSection.appendChild(thumbsUpButton);
+                commentSection.appendChild(thumbsDownButton);
+            };
+        });
     })
     .catch(error => {
         console.error("Error:", error);
